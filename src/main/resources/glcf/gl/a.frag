2d4b4f1e1d327c4789ec8827fc527b75ed8087a4
onasu <nanachi7711@outlook.jp>
2024-03-04 18:22:59 +0900

    動いた2

--- src/main/resources/glcf/gl/a.frag
+++ src/main/resources/glcf/gl/a.frag
@@ -1,9 +1,11 @@
 #version 150 core
 
+//in sampler2D samler;
+
 in vec4 vColor;
 
 out vec4 glColor;
 
 void main() {
-    gl_FragColor = vec4(0, 1, 0, 1);
+    glColor = vec4(vColor);
 }
--- src/test/java/Test000.java
+++ src/test/java/Test000.java
@@ -1,5 +1,6 @@
 import org.glcf.main.VAO;
 import org.glcf.main.*;
+import org.jetbrains.annotations.NotNull;
 
 import javax.imageio.ImageIO;
 import java.io.BufferedReader;
@@ -12,8 +13,38 @@ import static org.lwjgl.opengl.GL20.*;
 public class Test000 {
     public static void main(String[] args) throws Exception {
         Window w = new Window();
-        VFShader shader = shader();
+        VFShader shader = getShader();
         Texture texture = new Texture(ImageIO.read(new File("C:/Users/nanac/画像/背景とか/GFamvg7b0AARtIv-c.jpg")));
+
+        VAO vao = getVao();
+
+        Attribute[] atts = vao.attributes();
+        shader.compile(vao);
+
+        Model m = new Model(vao) {
+            @Override
+            public void drawing() {
+                shader.use();
+
+                vao.draw(GL_TRIANGLES);
+
+                shader.disuse();
+            }
+        };
+        m.setDrawMode(GL_TRIANGLES);
+        m.setShader(shader);
+        m.setTexture(texture);
+
+        w.add(m);
+        w.run();
+    }
+
+    @NotNull
+    private static VAO getVao() {
+        IBO ibo = new IBO(
+                0, 1, 2,
+                2, 3, 0
+        );
 
         VBO pos = new VBO();
         pos.attach(
@@ -31,30 +62,24 @@ public class Test000 {
                 1, 0, 0, 1f
         );
 
-        IBO ibo = new IBO(
-                0, 1, 2,
-                2, 3, 0
+        VBO tex = new VBO();
+        tex.attach(
+                0, 0,
+                1, 0,
+                1, 1,
+                0, 1f
         );
 
-        VAO vao = new VAO(2);
+        VAO vao = new VAO(3);
         vao.attach(0, pos, new Attribute(3, GL_FLOAT, 0, false, "pos"));
         vao.attach(1, color, new Attribute(4, GL_FLOAT, 0, false, "color"));
+        vao.attach(2, tex, new Attribute(2, GL_FLOAT, 0, false, "tex"));
         vao.setFragdata(new Attribute(4, GL_FLOAT, 0, false, "glColor"));
         vao.attach(ibo);
-
-        Attribute[] atts = vao.attributes();
-        shader.compile(vao);
-
-        Model m = new Model(vao);
-        m.setDrawMode(GL_TRIANGLES);
-        m.setShader(shader);
-        m.setTexture(texture);
-
-        w.add(m);
-        w.run();
+        return vao;
     }
 
-    public static VFShader shader() {
+    public static VFShader getShader() {
         StringBuilder v = new StringBuilder();
         StringBuilder f = new StringBuilder();
         try (BufferedReader vin = new BufferedReader(new InputStreamReader(Test000.class.getResourceAsStream("glcf/gl/a.vert")));
